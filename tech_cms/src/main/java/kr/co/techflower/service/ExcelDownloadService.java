2a421d31d5d84ddb1d072a1c90e93d936c6cbdb5
leejj235 <leejj235@1c8ed19d-3234-0510-a5b4-b5c0b053d990>
2016-11-28 02:16:26 +0000

    git-svn-id: https://dev.naver.com/svn/slaves@65 1c8ed19d-3234-0510-a5b4-b5c0b053d990

--- tech_cms/src/main/java/kr/co/techflower/service/ExcelDownloadService.java
+++
@@ -1,9 +0,0 @@
-package kr.co.techflower.service;
-
-import java.util.Map;
-
-public interface ExcelDownloadService {
-
-	public Map<String, Object> createExcelFileUser(Map<String, Object> map) throws Exception;
-	
-}
